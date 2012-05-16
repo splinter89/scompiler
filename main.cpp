1b19066ca0f8ecf4c5b82ab897d45a160416114c
splinter <splinta89@gmail.com>
2012-05-16 16:58:11 +0300

    +loading time msg

--- main.cpp
+++ main.cpp
@@ -5,9 +5,14 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QTime loadingTimer;
+    loadingTimer.start();
+
     // init
     MainWindow w;
     w.show();
+
+    qDebug() << QString("loading time: %1 sec").arg(QString::number(loadingTimer.elapsed() / 1000., 'f', 3));
 
     return a.exec();
 }
