85ecd19380b9ef8abb0b5a08a3a82658ccc6a4ed
splinter <splinta89@gmail.com>
2019-01-09 23:32:22 -0800

    prepare zip/exe in deploy scripts

--- scripts/deploy_dynamic.bat
+++ scripts/deploy_dynamic.bat
@@ -1,8 +1,17 @@
 @echo off
+
 cd ..\..\scompiler-build-desktop
 if not exist "deploy" mkdir deploy
 copy release\scompiler.exe deploy\
+cd deploy
+
 set PATH=C:\Qt\5.11.3\mingw53_32\bin;C:/Qt/Tools/mingw530_32\bin;%PATH%
-C:\Qt\5.11.3\mingw53_32\bin\windeployqt.exe deploy\scompiler.exe
-upx deploy\scompiler.exe
+C:\Qt\5.11.3\mingw53_32\bin\windeployqt.exe scompiler.exe
+
+"C:\Program Files\upx\upx.exe" scompiler.exe
+
+set RELEASE_FILENAME=scompiler_qt5_mingw32bit_dynamic.zip
+if exist ..\..\%RELEASE_FILENAME% del ..\..\%RELEASE_FILENAME%
+"C:\Program Files\WinRAR\WinRAR.exe" a -afzip -r ..\..\%RELEASE_FILENAME% .
+
 REM pause
--- scripts/deploy_static.bat
+++ scripts/deploy_static.bat
@@ -1,6 +1,14 @@
 @echo off
+
 cd ..\..\scompiler-build-desktop-static
 if not exist "deploy" mkdir deploy
 copy release\scompiler.exe deploy\
-upx deploy\scompiler.exe
+cd deploy
+
+"C:\Program Files\upx\upx.exe" scompiler.exe
+
+set RELEASE_FILENAME=scompiler_qt5_mingw32bit_static.exe
+if exist ..\..\%RELEASE_FILENAME% del ..\..\%RELEASE_FILENAME%
+copy scompiler.exe ..\..\%RELEASE_FILENAME%
+
 REM pause
