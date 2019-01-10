4919a0e5607d2bb31020fcf69d3ec1011de46bbb
splinter <splinta89@gmail.com>
2019-01-09 22:45:48 -0800

    (temp) init grammar and restore one dummy rule - for release

--- src/basics.h
+++ src/basics.h
@@ -482,7 +482,8 @@ static QList<GrammarRule> initGrammarFullRules()
          << GrammarRule(N_BRANCHING, EmptyTokenList()
                                        << K_IF << S_ROUND_OPEN << N_EXPRESSION << S_ROUND_CLOSE << N_BLOCK)
          << GrammarRule(N_BRANCHING, EmptyTokenList() << K_IF << S_ROUND_OPEN << N_EXPRESSION << S_ROUND_CLOSE
-                                                      << N_BLOCK << K_ELSE << N_BLOCK);
+                                                      << N_BLOCK << K_ELSE << N_BLOCK)
+         << GrammarRule(N_EXPRESSION, EmptyTokenList() << N_OP_9);
 
     return list;
 }
